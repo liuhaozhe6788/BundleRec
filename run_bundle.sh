43e1dd9573d73b02142fa08700277e228d27e924
liuhaozhe6788 <2792382045@qq.com>
2023-07-05 15:55:27 +0800

    new commits

--- run_bundle.sh
+++ run_bundle.sh
@@ -1,3 +1,3 @@
-python main.py --dataset bundle --batch_size 512 --lr 1e-3 --transformer_num 1  --embedding 8 --num_head 8  --time_aware True --seed 0 --data_path ./data/bundle_time --max_len 8 --force_restart  > "bundle_train.log" 2>&1
+# python main.py --dataset bundle --batch_size 512 --lr 1e-3 --transformer_num 1  --embedding 8 --num_head 8 --seed 0 --data_path ./data/bundle_time --max_len 8 --force_restart  > "bundle_train.log" 2>&1
 
-python main.py --dataset bundle --batch_size 512 --lr 1e-3 --transformer_num 1  --embedding 8 --num_head 8  --time_aware True --seed 0 --data_path ./data/bundle_time --max_len 8   > "bundle_test.log" 2>&1
+python main.py --dataset bundle --batch_size 512 --lr 1e-3 --transformer_num 1  --embedding 8 --num_head 8 --seed 0 --data_path ./data/bundle_time --max_len 8   > "bundle_test.log" 2>&1
